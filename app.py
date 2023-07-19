83e6ebc1ae1454234bf4ab5630f43a33f3e4eeed
Abhishek Pandey <ajaxpandey1234@gmail.com>
2023-07-19 18:11:53 +0545

    inintial simple demo app

--- app.py
+++ app.py
@@ -1,13 +1,66 @@
 import streamlit as st
 from streamlit_chat import message
+import pandas as pd
+import json
+from Scripts.agent.analyst_agent import create_agent , query_agent
+
+def decode_response(response: str) -> dict:
+    """This function converts the string response from the model to a dictionary object.
+
+    Args:
+        response (str): response from the model
+
+    Returns:
+        dict: dictionary with response data
+    """
+    return json.loads(response)
+
+def write_response(response_dict: dict):
+    """
+    Write a response from an agent to a Streamlit app.
+
+    Args:
+        response_dict: The response from the agent.
+
+    Returns:
+        None.
+    """
+    if "answer" in response_dict:
+        st.write(response_dict["answer"])
+
+    if "bar" in response_dict:
+        data = response_dict["bar"]
+        df = pd.DataFrame(data)
+        df.set_index("columns", inplace=True)
+        st.bar_chart(df)
+
+    if "line" in response_dict:
+        data = response_dict["line"]
+        df = pd.DataFrame(data)
+        df.set_index("columns", inplace=True)
+        st.line_chart(df)
+
+    if "table" in response_dict:
+        data = response_dict["table"]
+        df = pd.DataFrame(data["data"], columns=data["columns"])
+        st.table(df)
 
 st.title("Smart Business assistant")
 
-def get_text():
-    input_text = st.text_input("You: ", "Hello, how are you?", key="input")
-    return input_text
+st.write("Please upload your CSV file below.")
 
+data = st.file_uploader("Upload a CSV")
 
-user_input = get_text()
+query = st.text_area("Insert your query")
+
+if st.button("Submit Query", type="primary"):
+    agent = create_agent(data)
+
+    response = query_agent(agent=agent, query=query)
+
+    decoded_response = decode_response(response)
+
+    write_response(decoded_response)
+
 
 
